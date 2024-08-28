7d0024e6b5daed8cc9125f62026c695158dad30c
taylorrubbert <tayrubbert@hotmail.com>
2024-08-27 19:06:57 -0500

    weathercard started

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -3,3 +3,15 @@
 // weather data not stored in Main, need to pass it down from App component
 
 //weather card receives data from its parent (props chain example: App → Main → WeatherCard)
+import "./weathercard.css";
+import sunny from "../../images/sunny.png";
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp"></p>
+      <img className="weather-card__image" src={sunny} alt="sunny" />
+    </section>
+  );
+}
+
+export default WeatherCard;
